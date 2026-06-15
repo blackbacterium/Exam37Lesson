822854940c0abf8000ed0369ea8caf2d820d4601
Roman <pimkin-roma@mail.ru>
2026-06-15 20:52:10 +0300

    создал проект

---
+++ Exam37Lesson/Exam37Lesson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exam37Lesson
+//
+//  Created by Black Bacterium on 15.06.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
